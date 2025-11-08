d1d6ec096604f235a72d913f996406fdc0012363
Aryan <mishraaryan6387@gmail.com>
2025-11-08 10:16:50 +0530

    new file

---
+++ beg.js
@@ -0,0 +1,14 @@
+// console.log("Hello world");
+// console.log("hello"); 
+// console.log(23*23);
+
+console.log("aryan mishra");
+b = 5;
+console.log(b,  typeof(b));
+let a = 5;
+console.log(b , typeof(a) ); 
+
+age = 56;
+if( age >=5 )
+    console.log(" adult");
+else console.log("child");
