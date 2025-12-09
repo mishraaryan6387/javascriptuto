79b70b2aa1e1ad441b859f8f39f68eb271a7be7f
Aryan <mishraaryan6387@gmail.com>
2025-12-09 21:31:36 +0530

    `Added new code to primitiive.js, including string manipulation and object creation.`

---
+++ nums&math.js
@@ -0,0 +1,29 @@
+// const balance = 1000;
+
+// console.log(typeof(balance));
+
+// console.log(balance.toString());
+
+// console.log(typeof(balance));
+
+
+// // +++++++++++ maths ++++++++
+
+// const Math ;
+
+
+// console.log(Math.abs(-4));
+
+
+// console.log(Maths.a
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const max = 20;
+const min = 10;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+ 
+
--- primitiive.js
+++ primitiive.js
@@ -7,12 +7,46 @@
 
 // array, Objects, Funtions
 
-const heroes = ["shhaktimman", "naagraj", "doga"];
-let myObj = {
-    name = "Aryan mishra";
-    age = 22;
-}
+const heroes = ["shaktimman", "naagraj", "doga"];
 
-console.log function =(){
-    console.log();
+
+let myObj = {
+    name : "Aryan mishra",
+    age : 22
+
 }
+const name = "aryan mmishra";
+const age = 22;
+console.log( `hello ny name is ${name} and my age is ${age}`);
+const gameName =  new String ("hieshhc");
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+// game length
+console.log(gameName.length);
+
+// upper case 
+console.log(gameName.toUpperCase());
+
+// char at
+console.log(gameName.charAt(2));
+
+// index of
+console.log(gameName.indexOf('h'));
+
+const newString = gameName.substring(0,4);
+
+console. log(newString);
+// slice
+console.log(newString.slice(-4,1));
+
+const newString2 = newString;
+// trim 
+
+console.log(newString2.trim());
+
+const url = " https"
+
+
+// memory
+// stack ( primative), Heap (Non - Primitive) 
