064dc0c5c4c606cbd2e2473083dfa1dab8b39212
Aryan <mishraaryan6387@gmail.com>
2026-02-23 00:04:32 +0530

    {commit_message}

--- beg.js
+++ beg.js
@@ -1,4 +1,22 @@
-let gameCounter = 100;
-gameCounter++;
-console.log(gameCounter); 
-console.log("2" === 2);
+const descripter =Object.getOwnPropertyDescriptor(Math,"PI");
+
+console.log(descripter);
+
+console.log(Math.PI);
+
+
+const obj1 = {
+    name : "Alice",
+
+}
+
+console.log(Object.getOwnPropertyDescriptor(obj1,"name"));
+Object.defineProperties(obj1,{
+    name : {
+        writable : false,
+        enumerable : false,
+        configurable : false
+    }
+});
+
+console.log(Object.getOwnPropertyDescriptor(obj1,"name"));
